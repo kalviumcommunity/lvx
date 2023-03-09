6f94f6a9037d8e946759edb37b915b27adff21ae
NihitYadav <nihityadav8@gmail.com>
2023-03-09 18:18:49 +0530

    Home page, Single item view, Sell page are made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Item from "./Components/Item/Item";
+import data from "./data.json";
+import "./App.css";
 function App() {
   return (
     <>
-      <Navbar />
+    <Navbar />
+      <div className="content">
+        {data.map((product, index) => (
+          <Item
+            key={index}
+            image={product.image}
+            price={product.price}
+            description={product.description}
+            id={product.id}
+          />
+        ))}
+      </div>
     </>
-    )
+  );
 }
 
-export default App
+export default App;
