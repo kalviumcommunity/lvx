86b8e1368908f42c2c5ec66adf2d5c2467c8087f
NihitYadav <nihityadav8@gmail.com>
2023-03-22 18:30:01 +0530

    backend initialised; routes created; get request

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Item from "./Components/Item/Item";
 import data from "./data.json";
+import { useEffect } from "react";
 import "./App.css";
 function App() {
+
+  const url = 'http://127.0.0.1:2000/lvx/items'
+  useEffect(()=>{
+
+    const fetchDB = async() =>{
+      try{
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    fetchDB()
+  },[])
   return (
     <>
     <Navbar />
